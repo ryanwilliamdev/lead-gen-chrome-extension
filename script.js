2a43e96242573642f58ceaba511b66593867ef7b
rywilldev <rywilldav@gmail.com>
2023-05-02 19:19:00 -0400

    update script.js
    added localStorage for the browser to remember items inputted into the myLeads array

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
-
+// localStorage.setItem("myLeads", "www.epiclead.com");
+// console.log(localStorage.getItem("myLeads"));
+localStorage.clear();
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
 });
 
