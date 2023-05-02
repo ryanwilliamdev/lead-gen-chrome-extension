0c70d13a0ae092ad16d7b46f383eb00a0438504a
rywilldev <rywilldav@gmail.com>
2023-05-02 13:52:46 -0400

    update final styling, update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
+};
