0440d0006b529a1a05ad096aa6f95f24e9558d6f
rywilldev <rywilldav@gmail.com>
2023-05-03 14:29:53 -0400

    final update and refactor

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
 let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
+const tabBtn = document.querySelector("#tab-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 const ulEl = document.querySelector("#ul-el");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-});
-
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
     listItems += `
     <li>
-        <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]}
+        <a target='_blank' href='${leads[i]}'>
+            ${leads[i]}
         </a>
     </li>
     `;
   }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
